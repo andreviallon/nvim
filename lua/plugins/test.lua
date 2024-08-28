ec0c246b73a6762bea4b40b472db21112cce45fd
André Viallon <viallon.andre@gmail.com>
2024-08-28 06:35:17 +0200

    Update config

---
+++ lua/plugins/test.lua
@@ -0,0 +1,18 @@
+return {
+  { "nvim-neotest/neotest-plenary" },
+  {
+    "nvim-neotest/neotest",
+    opts = { adapters = { "neotest-plenary" } },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "marilari88/neotest-vitest",
+    },
+    opts = {
+      adapters = {
+        ["neotest-vitest"] = {},
+      },
+    },
+  },
+}
