796888a33473feb703df85008cd2d0c787286d1a
Andre Viallon <avi@trackunit.com>
2025-03-17 12:49:44 +0100

    Switch to fzf

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -11,68 +11,20 @@ return {
     lazygit = { enabled = true },
     gitbrowse = { enabled = true },
     notifier = { enabled = true },
-    picker = { enabled = true },
   },
   -- stylua: ignore
   keys = {
     -- Top Pickers & Explorer
-    { "<leader><leader>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
-    { "<leader>.", function() Snacks.picker.buffers() end, desc = "Buffers" },
-    { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
-    { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
     {"<leader>e", function() Snacks.explorer.open() end, desc = "Explorer" },
-    {
-      "<leader>ft",
-      function()
-        local filetype = vim.fn.input 'Enter file extension:'
-        if filetype ~= '' then
-          Snacks.picker.grep { glob = '**/*.' .. filetype }
-        end
-      end,
-      desc = "Search by File Type" },
 
-    -- Buffers
+    -- -- Buffers
     { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
-
-    -- Files & Search
-    { "<leader>nn", function() Snacks.picker.notifications() end, desc = "Notification History" },
-    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
-    { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
-    { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
-    { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
-    { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
-    { "<leader>fR", function() Snacks.picker.resume() end, desc = "Resume" },
-    { "<leader>fu", function() Snacks.picker.undo() end, desc = "Undo History" },
-    { "<leader>fs", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
-    { "<leader>fS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
-    { "<leader>fk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
-
-    -- Diagnostics
-    { "<leader>dd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
 
     -- Git
     { "<leader>gg", function() Snacks.lazygit() end, desc = "LazyGit" },
     { "<leader>go", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
-    { "<leader>gB", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
-    { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
-    { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
-    { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
-    { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
-    { "<leader>gD", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
-    { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
-
-    -- LSP
-    { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
-    { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-    { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
-    { "gi", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
-    { "gt", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto Type Definition" },
 
     -- Notifier
     { "<leader>nd", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-
-    -- UI
-    { "<leader>uc", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
   },
 }
